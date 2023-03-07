174140b770a8f77f8dd3b5f330cb2122bb061d74
d4min <danishamin.rb@gmail.com>
2023-03-07 19:43:23 +0000

    milestone 4 task 2

--- manual_rps.py
+++ manual_rps.py
@@ -1,12 +1,12 @@
 import random 
 
 def get_computer_choice():
-    rps  = ["rock", "paper", "scissors"]
+    rps  = ["Rock", "Paper", "Scissors"]
     return random.choice(rps)
 
 def get_user_choice():
     user_choice = input("Please enter rock, paper or scissors")
-    return user_choice.lower()
+    return user_choice.capitalize()
 
 computer_choice = get_computer_choice()
 user_choice = get_user_choice()
@@ -14,25 +14,25 @@ user_choice = get_user_choice()
 def get_winner(computer_choice, user_choice):
     winner = ""
     if computer_choice == user_choice:
-        print("It's a tie")
+        print("It is a tie")
         winner = "Tie"
-    elif computer_choice == "rock":
-        if user_choice == "paper":
-            print("You won")
+    elif computer_choice == "Rock":
+        if user_choice == "Paper":
+            print("You won!")
             winner = "User"
         else:
             print("You lost")
             winner = "Computer"
-    elif computer_choice == "paper":
-        if user_choice == "scissors":
-            print("You won")
+    elif computer_choice == "Paper":
+        if user_choice == "Scissors":
+            print("You won!")
             winner = "User"
         else:
             print("Computer Wins")
             winner = "You lost"
-    elif computer_choice == "scissors":
-        if user_choice == "rock":
-            print("You won")
+    elif computer_choice == "Scissors":
+        if user_choice == "Rock":
+            print("You won!")
             winner = "User"
         else:
             print("You lost")
