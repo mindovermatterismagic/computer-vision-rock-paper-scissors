1eb2b4dc5c888f29a663c5f40afa3f0174333b94
d4min <danishamin.rb@gmail.com>
2023-03-07 20:32:03 +0000

    Milestone 4 Task 3

--- manual_rps.py
+++ manual_rps.py
@@ -1,44 +1,48 @@
 import random 
 
-def get_computer_choice():
-    rps  = ['Rock', 'Paper', 'Scissors']
-    return random.choice(rps)
 
-def get_user_choice():
-    user_choice = input('Please enter rock, paper or scissors')
-    return user_choice.capitalize()
+def play():
+
+    def get_computer_choice():
+        rps  = ['Rock', 'Paper', 'Scissors']
+        return random.choice(rps)
+
+    def get_user_choice():
+        user_choice = input('Please enter rock, paper or scissors')
+        return user_choice.capitalize()
 
 
-computer_choice = get_computer_choice()
-print(computer_choice)
-user_choice = get_user_choice()
+    computer_choice = get_computer_choice()
+    user_choice = get_user_choice()
 
-def get_winner(computer_choice, user_choice):
-    winner = ''
-    if computer_choice == user_choice:
-        print('It is a tie!')
-        winner = 'Tie'
-    elif computer_choice == 'Rock':
-        if user_choice == 'Paper':
-            print('You won!')
-            winner = 'User'
-        else:
-            print('You lost')
-            winner = 'Computer'
-    elif computer_choice == 'Paper':
-        if user_choice == 'Scissors':
-            print('You won!')
-            winner = 'User'
-        else:
-            print('You lost')
-            winner = 'Computer'
-    elif computer_choice == 'Scissors':
-        if user_choice == 'Rock':
-            print('You won!')
-            winner = 'User'
-        else:
-            print('You lost')
-            winner = 'Computer'
-    return winner
+    def get_winner(computer_choice, user_choice):
+        winner = ''
+        if computer_choice == user_choice:
+            print('It is a tie!')
+            winner = 'Tie'
+        elif computer_choice == 'Rock':
+            if user_choice == 'Paper':
+                print('You won!')
+                winner = 'User'
+            else:
+                print('You lost')
+                winner = 'Computer'
+        elif computer_choice == 'Paper':
+            if user_choice == 'Scissors':
+                print('You won!')
+                winner = 'User'
+            else:
+                print('You lost')
+                winner = 'Computer'
+        elif computer_choice == 'Scissors':
+            if user_choice == 'Rock':
+                print('You won!')
+                winner = 'User'
+            else:
+                print('You lost')
+                winner = 'Computer'
+        return winner
 
-get_winner(computer_choice, user_choice)
+    get_winner(computer_choice, user_choice)
+
+play()
