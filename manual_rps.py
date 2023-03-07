958a535f90fff2d457299c0ecbb32400bf509b06
d4min <danishamin.rb@gmail.com>
2023-03-07 19:59:19 +0000

    Milestone 4 Task 2

--- manual_rps.py
+++ manual_rps.py
@@ -1,42 +1,42 @@
 import random 
 
 def get_computer_choice():
-    rps  = ["Rock", "Paper", "Scissors"]
+    rps  = ['Rock', 'Paper', 'Scissors']
     return random.choice(rps)
 
 def get_user_choice():
-    user_choice = input("Please enter rock, paper or scissors")
+    user_choice = input('Please enter rock, paper or scissors')
     return user_choice.capitalize()
 
 computer_choice = get_computer_choice()
 user_choice = get_user_choice()
 
 def get_winner(computer_choice, user_choice):
-    winner = ""
+    winner = ''
     if computer_choice == user_choice:
-        print("It is a tie!")
-        winner = "Tie"
-    elif computer_choice == "Rock":
-        if user_choice == "Paper":
-            print("You won!")
-            winner = "User"
+        print('It is a tie!')
+        winner = 'Tie'
+    elif computer_choice == 'Rock':
+        if user_choice == 'Paper':
+            print('You won!')
+            winner = 'User'
         else:
-            print("You lost")
-            winner = "Computer"
-    elif computer_choice == "Paper":
-        if user_choice == "Scissors":
-            print("You won!")
-            winner = "User"
+            print('You lost')
+            winner = 'Computer'
+    elif computer_choice == 'Paper':
+        if user_choice == 'Scissors':
+            print('You won!')
+            winner = 'User'
         else:
-            print("Computer Wins")
-            winner = "You lost"
-    elif computer_choice == "Scissors":
-        if user_choice == "Rock":
-            print("You won!")
-            winner = "User"
+            print('Computer Wins')
+            winner = 'You lost'
+    elif computer_choice == 'Scissors':
+        if user_choice == 'Rock':
+            print('You won!')
+            winner = 'User'
         else:
-            print("You lost")
-            winner = "Computer"
+            print('You lost')
+            winner = 'Computer'
     return winner
 
 get_winner(computer_choice, user_choice)
